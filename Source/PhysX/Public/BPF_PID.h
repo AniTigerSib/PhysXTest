04b1c913b8e539af9138cb79f22d914efee6ae8e
AniTigerSib <khudoleev04@mail.ru>
2023-04-16 20:29:43 +0500

    0.1.0

---
+++ Source/PhysX/Public/BPF_PID.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BPF_PID.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PHYSX_API UBPF_PID : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
